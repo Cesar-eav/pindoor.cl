09e90d04f451dbe593e597aa77382366feff6eba
Cesar-eav <cesar.eav@hotmail.es>
2026-05-27 13:38:12 -0400

    v 0.0.1

--- resources/views/components/nav_lateral.blade.php
+++ resources/views/components/nav_lateral.blade.php
@@ -8,7 +8,7 @@
                 <span class="text-[#fc5648]">Pin</span><span class="text-gray-900">door</span>
             </span>
             <span class="text-[10px] text-gray-400 font-semibold tracking-wider uppercase mt-1">
-                Road House Blues
+                V 0.0.1
             </span>
         </a>
     </div>
