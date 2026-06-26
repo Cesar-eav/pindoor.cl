d6b271c489ad671d677c01d56388a38f4c02681b
Cesar-eav <cesar.eav@hotmail.es>
2026-06-26 18:41:49 -0400

    Block contador

--- resources/views/layouts/pindoor.blade.php
+++ resources/views/layouts/pindoor.blade.php
@@ -27,16 +27,17 @@
     @livewireStyles
     @yield('head')
     @if(app()->environment('production'))
-    <script>(function(c,l,a,r,i,t,y){c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);})(window,document,"clarity","script","wajfuymjy1");</script>
-    <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','GTM-K7X98GZ8');</script>
-    @endif
-
     <script>
-  // Si la URL contiene ?exclude=true, se crea la cookie
-  if (window.location.search.includes("exclude=true")) {
-    document.cookie = "clarity_ignore=true; path=/; SameSite=Lax";
-  }
-</script>
+    (function() {
+        if (window.location.search.includes('no_tracking=1')) {
+            document.cookie = 'no_tracking=1; path=/; max-age=31536000; SameSite=Lax';
+        }
+        if (document.cookie.indexOf('no_tracking=1') !== -1) return;
+        (function(c,l,a,r,i,t,y){c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);})(window,document,"clarity","script","wajfuymjy1");
+        (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','GTM-K7X98GZ8');
+    })();
+    </script>
+    @endif
 
 </head>
 <body class="font-sans @yield('bodyClass', 'bg-gray-100 text-gray-900')" style="padding-bottom: var(--inset-bottom, 0px);">
