33c617c8d83d508cf80cbfe43598adba8ebe68fc
Cesar-eav <cesar.eav@hotmail.es>
2026-08-19 23:15:42 -0400

    Sitemap actualizado

--- resources/views/sitemap.blade.php
+++ resources/views/sitemap.blade.php
@@ -36,4 +36,37 @@
     </url>
     @endforeach
 
+    @foreach($posts as $post)
+    <url>
+        <loc>{{ route('blog.show', $post->slug) }}</loc>
+        @if($post->publicado_en)
+        <lastmod>{{ $post->publicado_en->toAtomString() }}</lastmod>
+        @endif
+        <changefreq>monthly</changefreq>
+        <priority>0.6</priority>
+    </url>
+    @endforeach
+
+    @foreach($rutas as $ruta)
+    <url>
+        <loc>{{ route('rutas.show', $ruta->slug) }}</loc>
+        @if($ruta->publicado_en)
+        <lastmod>{{ $ruta->publicado_en->toAtomString() }}</lastmod>
+        @endif
+        <changefreq>monthly</changefreq>
+        <priority>0.6</priority>
+    </url>
+    @endforeach
+
+    @foreach($recomendaciones as $recomendacion)
+    <url>
+        <loc>{{ route('recomienda.show', $recomendacion->slug) }}</loc>
+        @if($recomendacion->publicado_en)
+        <lastmod>{{ $recomendacion->publicado_en->toAtomString() }}</lastmod>
+        @endif
+        <changefreq>monthly</changefreq>
+        <priority>0.6</priority>
+    </url>
+    @endforeach
+
 </urlset>
